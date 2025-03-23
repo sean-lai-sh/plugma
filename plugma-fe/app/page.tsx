caf960ce50bdbcff237e7ce4177686db7c676d35
Sean Lai <seanlai221@gmail.com>
2025-03-23 16:31:21 -0400

    died on making it run on AWS + fixed smth with clientside comps on landing page

--- plugma-fe/app/page.tsx
+++ plugma-fe/app/page.tsx
@@ -2,14 +2,9 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
 import Footer from "@/components/footer";
-import Image from "next/image";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { supabase } from "../lib/supabaseClient";
+
 
 export default function Home() {
-  const [user, setUser] = useState<any>(null);
-  const router = useRouter();
   return (
     <div className="min-h-screen bg-white">
       <Navbar/>
