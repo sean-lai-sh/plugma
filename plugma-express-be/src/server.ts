9bed50fbd920d4bded0175b201c43d9583d25e7c
Sean Lai <seanlai221@gmail.com>
2025-03-28 11:45:18 -0400

    changed logo

--- plugma-express-be/src/server.ts
+++ plugma-express-be/src/server.ts
@@ -1,4 +1,6 @@
 import app from "./app";
 
 const PORT: number = Number(process.env.PORT) || 8000;
+
 app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server Running on ${PORT}`));
+app.set('trust proxy', true);
--- plugma-fe/app/layout.tsx
+++ plugma-fe/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     images: ["/og-image.png"],
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.svg",
   },
 };
 
